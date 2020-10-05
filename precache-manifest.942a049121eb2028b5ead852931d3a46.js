2d2c560fd8f293314f09cbf5571c101f974f3724
bharathjinka09 <bharathjinka09@gmail.com>
2020-10-05 10:58:28 +0530

    Updates

---
+++ precache-manifest.942a049121eb2028b5ead852931d3a46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e517477015318a32c9fde779efafe8b9",
+    "url": "/react-sliding-puzzle/index.html"
+  },
+  {
+    "revision": "16024e18fcffbcf4548a",
+    "url": "/react-sliding-puzzle/static/css/main.a1c8720a.chunk.css"
+  },
+  {
+    "revision": "4faf7e7c5b19a2bcfea5",
+    "url": "/react-sliding-puzzle/static/js/2.740e623c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-sliding-puzzle/static/js/2.740e623c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16024e18fcffbcf4548a",
+    "url": "/react-sliding-puzzle/static/js/main.27c318a0.chunk.js"
+  },
+  {
+    "revision": "4d2b0d54c1827d8317e7",
+    "url": "/react-sliding-puzzle/static/js/runtime-main.a71b23c6.js"
+  }
+]);
